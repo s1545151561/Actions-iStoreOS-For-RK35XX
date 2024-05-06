68bbce500ddfe059a16bd5c1527dda20a3ce8a7e
s1545151561 <141800803+s1545151561@users.noreply.github.com>
2024-05-06 20:15:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,14 +7,17 @@
 # Blog: https://p3terx.com
 #===============================================
 
-# 修改默认IP
- sed -i 's/192.168.10.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+# 修改openwrt登陆地址,把下面的 192.168.51.1 修改成你想要的就可以了
+  sed -i 's/192.168.100.1/192.168.51.1/g' package/base-files/files/bin/config_generate
+
+# 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
+  sed -i 's/OpenWrt/iStore OS/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # TTYD 免登录
- sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+  sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${GITHUB_WORKSPACE}/openwrt/package/feeds/packages/ttyd/files/ttyd.config
  
 # 添加额外插件
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
